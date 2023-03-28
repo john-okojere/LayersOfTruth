96deea6e3f8038a5f788e97d1aeb440bdcbc5e70
john-okojere <johnokojere08@gmail.com>
2023-03-28 19:46:46 +0100

    jj

--- siloam/admin.py
+++ siloam/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 from .models import Attendee, Attendance, Contact, CreateAttendance, WorkerAttendance, PastorAttendance
 
-admin.site.register(Attendee)
+
+@admin.register(Attendee)
+class AttendeAdmin(admin.ModelAdmin):
+    list_display = ('seat_number', 'full_name', 'phone', 'local_assembly', 'age', 'gender', 'state', 'accomodation', 'location', 'created')
+    list_filter = ('full_name', 'age', 'gender', 'accomodation', 'local_assembly')
+    search_fields = ('full_name', 'age', 'gender', 'accomodation', 'local_assembly')
+    ordering = ('seat_number',)
+
 admin.site.register(CreateAttendance)
 admin.site.register(WorkerAttendance)
 admin.site.register(PastorAttendance)
