e2a73a79c43381618a4a894bee4a32d078f45fed
john-okojere <johnokojere08@gmail.com>
2023-04-04 03:47:24 +0100

    forgot password

--- backend/settings.py
+++ backend/settings.py
@@ -162,7 +162,6 @@ LOGOUT_URL = '/account/Logout'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'mail.layersoftruth.org'
 EMAIL_USE_TLS = True
-EMAIL_USE_SSL = True
 EMAIL_PORT = 465
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
