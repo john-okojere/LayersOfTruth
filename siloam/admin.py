67dcc8f3901d94bc71c3413e505d38592b12402a
john-okojere <johnokojere08@gmail.com>
2023-04-07 07:17:38 +0100

    hh

--- siloam/admin.py
+++ siloam/admin.py
@@ -4,10 +4,10 @@ from .models import Attendee, Attendance, Contact, CreateAttendance, WorkerAtten
 
 @admin.register(Attendee)
 class AttendeAdmin(admin.ModelAdmin):
-    list_display = ('seat_number', 'full_name', 'phone', 'local_assembly', 'age', 'gender', 'state', 'accomodation', 'location', 'created')
+    list_display = ('id','seat_number', 'full_name', 'phone', 'local_assembly', 'age', 'gender', 'state', 'accomodation', 'location', 'created')
     list_filter = ('full_name', 'age', 'gender', 'accomodation', 'local_assembly')
-    search_fields = ('full_name', 'age', 'gender', 'accomodation', 'local_assembly')
-    ordering = ('seat_number',)
+    search_fields = ('id','full_name', 'age', 'gender', 'accomodation', 'local_assembly')
+    ordering = ('id',)
 
 admin.site.register(Specialcard)
 admin.site.register(CreateAttendance)
